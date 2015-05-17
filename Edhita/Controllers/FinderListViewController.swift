5afb8d0339e890c8656c8a9b2022b4c2f8b417a1
dapperstout <sir@dapperstout.com>
2015-05-17 15:15:11 +0200

    Remove advertising

--- Edhita/Controllers/FinderListViewController.swift
+++ Edhita/Controllers/FinderListViewController.swift
@@ -9,46 +9,4 @@
 import UIKit
 
 class FinderListViewController: EDHFinderListViewController {
-
-    var bannerView: GADBannerView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.initAd()
-    }
-
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-
-    // MARK: - Utilities
-    
-    func initAd() {
-        if UIDevice.currentDevice().orientation.isLandscape {
-            self.bannerView = GADBannerView(adSize: kGADAdSizeSmartBannerLandscape)
-        } else {
-            self.bannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
-        }
-        
-//        if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-//            self.bannerView = GADBannerView(adSize: kGADAdSizeMediumRectangle)
-//        } else {
-//            self.bannerView = GADBannerView(adSize: kGADAdSizeBanner)
-//        }
-        
-        self.bannerView.adUnitID =  AppSecret.Ad.AdMob.unitId
-        self.bannerView.rootViewController = self
-        
-        let request = GADRequest()
-        request.testDevices = [GAD_SIMULATOR_ID]
-        self.bannerView.loadRequest(request)
-        
-        self.tableView.tableFooterView = self.bannerView
-    }
-
 }
